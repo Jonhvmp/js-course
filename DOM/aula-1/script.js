0632c87ace023172bf2b493884c0ce4642148c4f
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2024-10-18 18:35:35 -0300

    Add DOM manipulation functionality

---
+++ DOM/aula-1/script.js
@@ -0,0 +1,20 @@
+// Aula de Manipulação de DOM
+
+// Selecionando um elemento a partir do ID
+let botao = document.getElementById('meuBotao');
+console.log(botao);
+
+// Selecionando um elemento a partir da classe
+let paragrafo = document.getElementsByClassName('meuParagrafo');
+console.log(paragrafo);
+
+// Selecionando um elemento a partir da tag
+let headers = document.getElementsByTagName('h1');
+console.log(headers);
+
+// Selecionando um elemento a partir do seletor
+let primeiroPargrafo = document.querySelector('p');
+console.log(primeiroPargrafo);
+
+let todosParagrafos = document.querySelectorAll('p');
+console.log(todosParagrafos);
