4726b5c526babbb596e9a42d99c745885a05fa7b
Jonh Alex <122692601+Jonhvmp@users.noreply.github.com>
2024-10-31 16:38:59 -0300

    Melhorar a interação do usuário com a busca de personagens, adicionando validação de entrada e permitindo envio com a tecla Enter. Estilizar a exibição das informações do personagem para melhor apresentação.

--- API/personagemResponse/script.js
+++ API/personagemResponse/script.js
@@ -1,6 +1,11 @@
 const button = document.getElementById("get-btn");
 const div = document.getElementById("data-container");
 
+document.onkeydown = function(event) {
+    if (event.key === 'Enter') button.click();
+};
+// permite que o usuário pressione Enter para enviar o formulário
+
 function getData() {
     const characterId = document.getElementById("character-id").value;
     if (!characterId) {
@@ -12,12 +17,15 @@ function getData() {
     xhr.open("GET", `https://rickandmortyapi.com/api/character/${characterId}`);
     xhr.send();
 
-    xhr.onload = function () {
+    xhr.onload = function () { // xhr.onload é chamado quando a requisição é completada
         if (xhr.status === 200) {
             const character = JSON.parse(xhr.response);
             div.innerHTML = `
-                <div class="character-info">
+                <article class="character-info">
+                    <div class="character-info__image">
                     <img src="${character.image}" alt="${character.name}">
+                    </div>
+                    <div class="character-info__details">
                     <h2>${character.name}</h2>
                     <p><strong>Status:</strong> ${character.status}</p>
                     <p><strong>Species:</strong> ${character.species}</p>
@@ -25,13 +33,14 @@ function getData() {
                     <p><strong>Origin:</strong> ${character.origin.name}</p>
                     <p><strong>Location:</strong> ${character.location.name}</p>
                 </div>
+                </article>
             `;
         } else {
             div.innerText = "Personagem não encontrado. Tente outro ID.";
         }
     };
 
-    xhr.onerror = function () {
+    xhr.onerror = function () { // quando ocorre erro na requisição
         div.innerText = "Erro ao buscar dados. Verifique sua conexão.";
     };
 }
